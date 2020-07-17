8d890587cd0fcbd20510f6beba83fee1df8b068c
baba_s <shotababa1206@gmail.com>
2020-07-17 21:08:49 +0900

    first commit

---
+++ Editor/ParentPositionResetter.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	/// <summary>
+	/// 親オブジェクトの位置のみリセットするエディタ拡張
+	/// </summary>
+	internal static class ParentPositionResetter
+	{
+		[MenuItem( "CONTEXT/Transform/Reset Position Only Parent" )]
+		private static void Reset( MenuCommand command )
+		{
+			var parent     = ( Transform ) command.context;
+			var tempParent = new GameObject();
+			var children   = parent.Cast<Transform>().ToArray();
+
+			var parentAndChildren = children
+					.Prepend( parent )
+					.OfType<Object>()
+					.ToArray()
+				;
+
+			Undo.RecordObjects( parentAndChildren, "Reset Position Only Parent" );
+
+			foreach ( var child in children )
+			{
+				child.parent = tempParent.transform;
+			}
+
+			parent.position = Vector3.zero;
+
+			foreach ( var child in children )
+			{
+				child.parent = parent;
+			}
+
+			Object.DestroyImmediate( tempParent );
+		}
+	}
+}
